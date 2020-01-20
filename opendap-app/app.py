5cd7fbf03dbbac5512357f8e265082874b9858cb
amarouane <amarouane@itsc.uah.edu>
2020-01-20 12:07:54 -0600

    One step closer

--- opendap-app/app.py
+++ opendap-app/app.py
@@ -1,19 +1,43 @@
 from flask import Flask, request
 from requests import get
 import boto3
-from os import path
+from os import path, listdir
+from pathlib import Path
 
 import subprocess
 app = Flask(__name__)
+s3 = boto3.client('s3')
 
 
+def return_time_left(bucket_name, s3_file_path, file_path, calibre_per_megabyte=4):
+    """
+    1 MG took 4 sec
+    """
+    response = s3.head_object(Bucket=bucket_name, Key=s3_file_path)
+    real_size = float("{0:.2f}".format(response['ContentLength']/(1024*1024)))
+    size_in_bytes = path.getsize(file_path)
+    size_so_far = float("{0:.2f}".format(size_in_bytes/(1024*1024)))
+    if real_size - size_so_far:
+        time_left = float("{0:.2f}".format((real_size - size_so_far) * calibre_per_megabyte))
+        return {'Status': 'Download in progress',
+                'Time left': f"{time_left} seconds"}
+    else:
+        return {'Status': "Download Complete!", 'Info': f"Check {request.remote_addr}:8080/opendap/data/ghrc_data/{s3_file_path}.html"}
 
-def use_boto3(file_name):
-    s3 = boto3.client('s3')
-    file_path = path.dirname(file_name)
-    run_command(f"mkdir -p {file_path}")
-    final_path = path.join('/usr/share/hyrax/data/ghrc_data', file_name)
-    return s3.download_file('amarouane-opendap-data', file_name, final_path)
+
+def use_boto3(sub_s3_file_path, bucket_name='amarouane-opendap-data'):
+    file_name = path.basename(sub_s3_file_path)
+    file_path = path.dirname(sub_s3_file_path)
+    final_path = path.join('/usr/share/hyrax/data/ghrc_data', file_path)
+    destination = path.join(final_path, file_name)
+    Path(final_path).mkdir(parents=True, exist_ok=True)
+    list_dir = listdir(final_path)
+    for ele in list_dir:
+        if file_name in ele:
+            return return_time_left(bucket_name, sub_s3_file_path, path.join(final_path, ele))
+
+    s3.download_file(bucket_name, sub_s3_file_path, destination)
+    return return_time_left(bucket_name, sub_s3_file_path, destination)
 
 def run_command(cmd):
     """
